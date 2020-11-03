40c7f5a90f8076dcbebddb6cd5f62c37d7380f2e
Robert <mail@rbeekman.nl>
2020-11-03 17:33:02 +0100

    WS281X - UI WIP

--- src/Devices/Artemis.Plugins.Devices.WS281X/ViewModels/WS281XConfigurationViewModel.cs
+++ src/Devices/Artemis.Plugins.Devices.WS281X/ViewModels/WS281XConfigurationViewModel.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Navigation;
 using Artemis.Core;
 using Artemis.Plugins.Devices.WS281X.Settings;
+using Artemis.Plugins.Devices.WS281X.ViewModels.Dialogs;
+using Artemis.UI.Shared.Services;
 
 namespace Artemis.Plugins.Devices.WS281X.ViewModels
 {
     public class WS281XConfigurationViewModel : PluginConfigurationViewModel
     {
+        private readonly IDialogService _dialogService;
         private PluginSetting<List<DeviceDefinition>> _definitions;
 
-        public WS281XConfigurationViewModel(Plugin plugin, PluginSettings settings) : base(plugin)
+        public WS281XConfigurationViewModel(Plugin plugin, PluginSettings settings, IDialogService dialogService) : base(plugin)
         {
+            _dialogService = dialogService;
             _definitions = settings.GetSetting<List<DeviceDefinition>>("DeviceDefinitions");
+        }
+
+        public void OpenHyperlink(object sender, RequestNavigateEventArgs e)
+        {
+            Utilities.OpenUrl(e.Uri.AbsoluteUri);
+        }
+
+        public void AddDevice()
+        {
+            _dialogService.ShowDialog<DeviceConfigurationDialog>();
+        }
+
+        public void EditDevice(DeviceDefinition device)
+        {
+            Dictionary<string, object> parameters = new Dictionary<string, object> {{"device", device}};
         }
     }
 }
