e0771140dee7d515777978854ca036ce97d75ad5
Diogo Trindade <diogotr7@gmail.com>
2021-01-07 23:34:57 +0000

    OpenRGB - Moved project

---
+++ src/Devices/Artemis.Plugins.Devices.OpenRGB/OpenRGBDeviceProvider.cs
@@ -0,0 +1,46 @@
+﻿using Artemis.Core;
+using Artemis.Core.DeviceProviders;
+using Artemis.Core.Services;
+using RGB.NET.Core;
+using RGB.NET.Devices.OpenRGB;
+using System.Collections.Generic;
+
+namespace Artemis.Plugins.Devices.OpenRGB
+{
+    public class OpenRGBDeviceProvider : DeviceProvider
+    {
+        private readonly IRgbService _rgbService;
+        private readonly PluginSettings _settings;
+
+        public OpenRGBDeviceProvider(IRgbService rgbService, PluginSettings settings) : base(RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.Instance)
+        {
+            _rgbService = rgbService;
+            _settings = settings;
+        }
+
+        public override void Enable()
+        {     
+            PluginSetting<List<OpenRGBServerDefinition>> definitions = _settings.GetSetting<List<OpenRGBServerDefinition>>("DeviceDefinitions");
+            if (definitions.Value is null)
+            {
+                definitions.Value = new List<OpenRGBServerDefinition>();
+                OpenRGBServerDefinition definition = new OpenRGBServerDefinition
+                {
+                    ClientName = "Artemis",
+                    Ip = "127.0.0.1",
+                    Port = 6742
+                };
+                definitions.Value.Add(definition);
+                definitions.Save();
+            }
+
+            foreach (OpenRGBServerDefinition def in definitions.Value)
+            {
+                RGB.NET.Devices.OpenRGB.OpenRGBDeviceProvider.Instance.DeviceDefinitions.Add(def);
+            }
+            PathHelper.ResolvingAbsolutePath += (sender, args) => ResolveAbsolutePath(typeof(AbstractOpenRGBDevice<>), sender, args);
+
+            _rgbService.AddDeviceProvider(RgbDeviceProvider);
+        }
+    }
+}
---
+++ src/Devices/Artemis.Plugins.Devices.OpenRGB/PluginBootstrapper.cs
@@ -0,0 +1,17 @@
+﻿using Artemis.Core;
+using Artemis.UI.Shared;
+
+namespace Artemis.Plugins.Devices.OpenRGB
+{
+    public class PluginBootstrapper : IPluginBootstrapper
+    {
+        public void Disable(Plugin plugin)
+        {
+        }
+
+        public void Enable(Plugin plugin)
+        {
+            plugin.ConfigurationDialog = new PluginConfigurationDialog<OpenRGBConfigurationDialogViewModel>();
+        }
+    }
+}
