e25c7859d9ff908a87772450396e862f8c06c72b
Cheerpipe <cheerpipe@gmail.con>
2021-03-16 09:11:19 -0300

    Add orientation and wave size properties to linear gradient
    Add base orientation property to easily create vertical and horizontal gradients.
    Add Wave Size property  to control how much of the gradient is actually visible (from the beginning of the gradient).

--- src/LayerBrushes/Artemis.Plugins.LayerBrushes.Color/PropertyGroups/LinearGradientBrushProperties.cs
+++ src/LayerBrushes/Artemis.Plugins.LayerBrushes.Color/PropertyGroups/LinearGradientBrushProperties.cs
@@ -11,6 +11,12 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         public IntLayerProperty ColorsMultiplier { get; set; }
 
         [PropertyDescription(Description = "Change the rotation of the gradient without affecting the rotation of the shape", InputAffix = "°")]
+        public IntLayerProperty WaveSize { get; set; }
+
+        [PropertyDescription(Description = "Change the base orientation of the gradient", InputAffix = "°")]
+        public EnumLayerProperty<LinearGradientOrientatonMode> Orientation { get; set; }
+
+        [PropertyDescription(Description = "Change the lenght of the visible portion of the gradient", InputAffix = "°")]
         public FloatLayerProperty Rotation { get; set; }
 
         [PropertyDescription(Description = "The speed at which the gradient moves vertically and horizontally in cm per second.", InputAffix = "cm/s")]
@@ -21,6 +27,8 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         protected override void PopulateDefaults()
         {
             Colors.DefaultValue = ColorGradient.GetUnicornBarf();
+            WaveSize.DefaultValue = 100;
+            Orientation.DefaultValue = LinearGradientOrientatonMode.Horizontal;
         }
 
         protected override void EnableProperties()
@@ -32,5 +40,11 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         }
 
         #endregion
+    }
+
+    public enum LinearGradientOrientatonMode
+    {
+        Horizontal,
+        Vertical
     }
 }
