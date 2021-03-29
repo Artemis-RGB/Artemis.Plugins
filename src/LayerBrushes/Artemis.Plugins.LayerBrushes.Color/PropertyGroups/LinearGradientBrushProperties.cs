d96455fb16951f8e8ae7e8b1d0369bd327ab93b0
Cheerpipe <cheerpipe@gmail.com>
2021-03-29 17:15:29 -0300

    Linear gradient additional properties (#27)
    * Linear gradient additional properties
    
    Add Wave Size (Replace Color repeater and allow below smaller color count than the gradient steps).
    Add Orientation (Vertical and Horizontal) to easily create vertical gradients without transformations.
    Add Mirror and Repeat Seamless modes
    
    * Meta - Updated launch settings and references to match Artemis changes
    
    * Linear gradient additional properties
    
    Add Wave Size (Replace Color repeater and allow below smaller color count than the gradient steps).
    Add Orientation (Vertical and Horizontal) to easily create vertical gradients without transformations.
    Add Mirror and Repeat Seamless modes
    
    * Color gradient - Simplified seamless code
    
    Color gradient - Typos
    
    Co-authored-by: Cheerpipe <cheerpipe@gmail.con>
    Co-authored-by: Robert <mail@rbeekman.nl>

--- src/LayerBrushes/Artemis.Plugins.LayerBrushes.Color/PropertyGroups/LinearGradientBrushProperties.cs
+++ src/LayerBrushes/Artemis.Plugins.LayerBrushes.Color/PropertyGroups/LinearGradientBrushProperties.cs
@@ -1,4 +1,5 @@
 ﻿using Artemis.Core;
+using SkiaSharp;
 
 namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
 {
@@ -7,10 +8,16 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         [PropertyDescription(Description = "The gradient of the brush")]
         public ColorGradientLayerProperty Colors { get; set; }
 
-        [PropertyDescription(Name = "Colors multiplier", Description = "How many times to repeat the colors in the selected gradient", DisableKeyframes = true, MinInputValue = 0, MaxInputValue = 10)]
-        public IntLayerProperty ColorsMultiplier { get; set; }
+        [PropertyDescription(Description = "Change the length of the visible portion of the gradient without change de size of the shape", InputAffix = "%")]
+        public IntLayerProperty WaveSize { get; set; }
 
-        [PropertyDescription(Description = "Change the rotation of the gradient without affecting the rotation of the shape", InputAffix = "°")]
+        [PropertyDescription(Description = "Change the orientation of the gradient without affecting the orientation of the shape")]
+        public EnumLayerProperty<LinearGradientOrientationMode> Orientation { get; set; }
+
+        [PropertyDescription(Description = "Change how the gradient will be handled when it is painted outside shape bounds")]
+        public EnumLayerProperty<LinearGradientRepeatMode> RepeatMode { get; set; }
+
+        [PropertyDescription(Description = "Change de rotation of the gradient without affecting the rotation of the shape", InputAffix = "°")]
         public FloatLayerProperty Rotation { get; set; }
 
         [PropertyDescription(Description = "The speed at which the gradient moves vertically and horizontally in cm per second.", InputAffix = "cm/s")]
@@ -21,6 +28,9 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         protected override void PopulateDefaults()
         {
             Colors.DefaultValue = ColorGradient.GetUnicornBarf();
+            WaveSize.DefaultValue = 100;
+            Orientation.DefaultValue = LinearGradientOrientationMode.Horizontal;
+            RepeatMode.DefaultValue = LinearGradientRepeatMode.Repeat;
         }
 
         protected override void EnableProperties()
@@ -32,5 +42,18 @@ namespace Artemis.Plugins.LayerBrushes.Color.PropertyGroups
         }
 
         #endregion
+    }
+
+    public enum LinearGradientOrientationMode
+    {
+        Horizontal,
+        Vertical
+    }
+
+    public enum LinearGradientRepeatMode
+    {
+        Repeat = 1,
+        RepeatSeamless = 2,
+        Mirror = 3
     }
 }
