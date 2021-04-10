0b4e2bedc2b749a0822af08268a2cee873a7d506
Darth Affe <darthaffe@wyrez.org>
2021-04-10 22:04:25 +0200

    AudioVisualization: Added Channel-Selection

--- src/LayerEffects/Artemis.Plugins.LayerEffects.AudioVisualization/AudioProcessing/Spectrum/FourierSpectrumProvider.cs
+++ src/LayerEffects/Artemis.Plugins.LayerEffects.AudioVisualization/AudioProcessing/Spectrum/FourierSpectrumProvider.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Artemis.Plugins.LayerEffects.AudioVisualization.AudioCapture;
 using MathNet.Numerics;
 using MathNet.Numerics.IntegralTransforms;
 
@@ -9,7 +8,7 @@ namespace Artemis.Plugins.LayerEffects.AudioVisualization.AudioProcessing.Spectr
     {
         #region Properties & Fields
 
-        private readonly AudioBuffer _audioBuffer;
+        private readonly IAudioDataProvider _dataProvider;
 
         private float[] _sampleData;
         private double[] _hamming;
@@ -18,13 +17,15 @@ namespace Artemis.Plugins.LayerEffects.AudioVisualization.AudioProcessing.Spectr
         private float[] _spectrum;
         private int _usableDataLength;
 
+        private bool _isDirty = true;
+
         #endregion
 
         #region Constructors
 
-        public FourierSpectrumProvider(AudioBuffer audioBuffer)
+        public FourierSpectrumProvider(IAudioDataProvider dataProvider)
         {
-            this._audioBuffer = audioBuffer;
+            this._dataProvider = dataProvider;
         }
 
         #endregion
@@ -33,28 +34,38 @@ namespace Artemis.Plugins.LayerEffects.AudioVisualization.AudioProcessing.Spectr
 
         public override void Initialize()
         {
-            _hamming = Window.Hamming(_audioBuffer.Size);
-            _sampleData = new float[_audioBuffer.Size];
-            _complexBuffer = new Complex32[_audioBuffer.Size];
-            _usableDataLength = (_audioBuffer.Size / 2) + 1;
+            _hamming = Window.Hamming(_dataProvider.BufferSize);
+            _sampleData = new float[_dataProvider.BufferSize];
+            _complexBuffer = new Complex32[_dataProvider.BufferSize];
+            _usableDataLength = (_dataProvider.BufferSize / 2) + 1;
             _spectrum = new float[_usableDataLength];
         }
 
         public override void Update()
         {
-            _audioBuffer.CopyMixInto(ref _sampleData, 0);
-
-            ApplyHamming(ref _sampleData);
-            CreateSpectrum(ref _sampleData);
+            _isDirty = true;
         }
 
-        private void ApplyHamming(ref float[] data)
+        private void CalculateSpectrum()
+        {
+            if (!_isDirty) return;
+
+            Span<float> data = new(_sampleData);
+            _dataProvider.GetDataInto(data);
+
+            ApplyHamming(data);
+            CreateSpectrum(data);
+
+            _isDirty = false;
+        }
+
+        private void ApplyHamming(in Span<float> data)
         {
             for (int i = 0; i < data.Length; i++)
                 data[i] = (float)(data[i] * _hamming[i]);
         }
 
-        private void CreateSpectrum(ref float[] data)
+        private void CreateSpectrum(in Span<float> data)
         {
             for (int i = 0; i < data.Length; i++)
                 _complexBuffer[i] = new Complex32(data[i], 0);
@@ -68,13 +79,29 @@ namespace Artemis.Plugins.LayerEffects.AudioVisualization.AudioProcessing.Spectr
             }
         }
 
-        public ISpectrum GetLinearSpectrum(int bands = 64, float minFrequency = -1, float maxFrequency = -1) => new LinearSpectrum(_spectrum, bands, minFrequency, maxFrequency);
+        public ISpectrum GetLinearSpectrum(int bands = 64, float minFrequency = -1, float maxFrequency = -1)
+        {
+            CalculateSpectrum();
+            return new LinearSpectrum(_spectrum, bands, minFrequency, maxFrequency);
+        }
 
-        public ISpectrum GetLogarithmicSpectrum(int bands = 12, float minFrequency = -1, float maxFrequency = -1) => new LogarithmicSpectrum(_spectrum, bands, minFrequency, maxFrequency);
+        public ISpectrum GetLogarithmicSpectrum(int bands = 12, float minFrequency = -1, float maxFrequency = -1)
+        {
+            CalculateSpectrum();
+            return new LogarithmicSpectrum(_spectrum, bands, minFrequency, maxFrequency);
+        }
 
-        public ISpectrum GetGammaSpectrum(int bands = 64, float gamma = 2, float minFrequency = -1, float maxFrequency = -1) => new GammaSpectrum(_spectrum, bands, gamma, minFrequency, maxFrequency);
+        public ISpectrum GetGammaSpectrum(int bands = 64, float gamma = 2, float minFrequency = -1, float maxFrequency = -1)
+        {
+            CalculateSpectrum();
+            return new GammaSpectrum(_spectrum, bands, gamma, minFrequency, maxFrequency);
+        }
 
-        public ISpectrum GetRawSpectrum() => new RawSpectrumProvider(_spectrum);
+        public ISpectrum GetRawSpectrum()
+        {
+            CalculateSpectrum();
+            return new RawSpectrumProvider(_spectrum);
+        }
 
         #endregion
     }
