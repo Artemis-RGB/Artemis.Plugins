a5b42db02bf9f69837d50b8adca2f56abb0fc5a1
Dennis Piecha <d.piecha@ymail.com>
2021-04-15 21:03:10 +0200

    add basic project structure

---
+++ src/Collections/Artemis.Plugins.Tasmota/RGB.NET/TasmotaRGBDeviceProvider.cs
@@ -0,0 +1,44 @@
+﻿using RGB.NET.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artemis.Plugins.Tasmota.RGB.NET
+{
+    public class TasmotaRGBDeviceProvider : AbstractRGBDeviceProvider
+    {
+        #region Constructors
+
+        public TasmotaRGBDeviceProvider()
+        {
+            if (_instance != null) throw new InvalidOperationException($"There can be only one instance of type {nameof(TasmotaRGBDeviceProvider)}");
+            _instance = this;
+        }
+
+        #endregion
+
+        #region Properties & Fields
+
+        private static TasmotaRGBDeviceProvider _instance;
+
+        public static TasmotaRGBDeviceProvider Instance => _instance ?? new TasmotaRGBDeviceProvider();
+
+        #endregion
+
+        #region Methods
+
+        protected override void InitializeSDK()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<IRGBDevice> LoadDevices()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Collections/Artemis.Plugins.Tasmota/TasmotaDeviceProvider.cs
@@ -0,0 +1,31 @@
+﻿using Artemis.Core.DeviceProviders;
+using Artemis.Core.Services;
+using Artemis.Plugins.Tasmota.RGB.NET;
+using System.IO;
+
+namespace Artemis.Plugins.Tasmota
+{
+    // This is your Artemis device provider, all it really does is act as a bridge between RGB.NET and Artemis
+    // You will not write any device logic in here, refer to the RgbNetDeviceProvider project instead    
+    public class TasmotaDeviceProvider : DeviceProvider
+    {
+        private readonly IRgbService _rgbService;
+
+        public TasmotaDeviceProvider(IRgbService rgbService) : base(TasmotaRGBDeviceProvider.Instance)
+        {
+            _rgbService = rgbService;
+        }
+
+        public override void Enable()
+        {
+
+            _rgbService.AddDeviceProvider(RgbDeviceProvider);
+        }
+
+        public override void Disable()
+        {
+            _rgbService.RemoveDeviceProvider(RgbDeviceProvider);
+            RgbDeviceProvider.Dispose();
+        }
+    }
+}
