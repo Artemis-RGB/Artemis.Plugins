0e36df78b292e8305d20dde18bec3d1c33516bb8
Robert <mail@rbeekman.nl>
2021-05-04 13:58:14 +0200

    Logitech - Added VC Redist prerequisite

--- src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.TestData/PluginBootstrapper.cs
+++ src/DataModelExpansions/Artemis.Plugins.DataModelExpansions.TestData/PluginBootstrapper.cs
@@ -1,17 +1,14 @@
 ﻿using Artemis.Core;
-using Artemis.Plugins.DataModelExpansions.TestData.Prerequisites;
 using Artemis.Plugins.DataModelExpansions.TestData.ViewModels;
 using Artemis.UI.Shared;
 
 namespace Artemis.Plugins.DataModelExpansions.TestData
 {
-    public class MyPluginBootstrapper : PluginBootstrapper
+    public class Bootstrapper : PluginBootstrapper
     {
         public override void OnPluginLoaded(Plugin plugin)
         {
             plugin.ConfigurationDialog = new PluginConfigurationDialog<TestPluginConfigurationViewModel>();
-            AddFeaturePrerequisite<PluginDataModelExpansion>(new TestPrerequisite1());
-            AddFeaturePrerequisite<PluginDataModelExpansion>(new TestPrerequisite2());
         }
     }
 }
