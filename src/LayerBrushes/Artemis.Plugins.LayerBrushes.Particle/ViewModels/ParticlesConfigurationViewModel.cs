c9bba5a33fab40e0e4b9d6a4683f9e71138840c6
Robert <mail@rbeekman.nl>
2021-05-30 12:38:16 +0200

    Particles - API update

--- src/LayerBrushes/Artemis.Plugins.LayerBrushes.Particle/ViewModels/ParticlesConfigurationViewModel.cs
+++ src/LayerBrushes/Artemis.Plugins.LayerBrushes.Particle/ViewModels/ParticlesConfigurationViewModel.cs
@@ -66,7 +66,7 @@ namespace Artemis.Plugins.LayerBrushes.Particle.ViewModels
 
         private void Save()
         {
-            _profileEditorService.UpdateSelectedProfileElement();
+            _profileEditorService.SaveSelectedProfileElement();
         }
 
         private void LoadConfiguration()
