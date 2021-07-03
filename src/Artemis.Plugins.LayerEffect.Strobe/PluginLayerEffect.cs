163e47edee109abf7fcda772d11d06d3ac4fc35b
Robert <mail@rbeekman.nl>
2021-07-03 15:27:33 +0200

    Strobe - Code cleanup
    Meta - Put back Resharper settings

--- src/Artemis.Plugins.LayerEffect.Strobe/PluginLayerEffect.cs
+++ src/Artemis.Plugins.LayerEffect.Strobe/PluginLayerEffect.cs
@@ -1,8 +1,8 @@
-﻿using Artemis.Core;
+﻿using System;
+using Artemis.Core;
 using Artemis.Core.LayerEffects;
 using Artemis.Plugins.LayerEffect.Strobe.PropertyGroups;
 using SkiaSharp;
-using System;
 
 namespace Artemis.Plugins.LayerEffect.Strobe
 {
@@ -14,7 +14,6 @@ namespace Artemis.Plugins.LayerEffect.Strobe
 
         public override void EnableLayerEffect()
         {
-
         }
 
         public override void DisableLayerEffect()
@@ -24,7 +23,7 @@ namespace Artemis.Plugins.LayerEffect.Strobe
         public override void Update(double deltaTime)
         {
             // Progress moves from 0 to 2 which is brought back to a value moving from 0 to 1 to 0 during pre-process
-            _progress = Mod((float)(_progress + (deltaTime * Properties.Speed * 2)), 2);
+            _progress = Mod((float) (_progress + deltaTime * Properties.Speed * 2), 2);
         }
 
         public override void PreProcess(SKCanvas canvas, SKRect renderBounds, SKPaint paint)
@@ -40,16 +39,16 @@ namespace Artemis.Plugins.LayerEffect.Strobe
                 if (Properties.BrightenTransitionMode.CurrentValue == StrobeTransitionMode.Linear)
                     alphaMultiplier = progress;
                 else if (Properties.BrightenTransitionMode.CurrentValue == StrobeTransitionMode.Eased)
-                    alphaMultiplier = (float)Easings.BounceEaseInOut(progress);
+                    alphaMultiplier = (float) Easings.BounceEaseInOut(progress);
                 else
                     alphaMultiplier = MathF.Round(progress, 0);
-            } 
+            }
             else
             {
                 if (Properties.DimTransitionMode.CurrentValue == StrobeTransitionMode.Linear)
                     alphaMultiplier = progress;
                 else if (Properties.DimTransitionMode.CurrentValue == StrobeTransitionMode.Eased)
-                    alphaMultiplier = (float)Easings.BounceEaseInOut(progress);
+                    alphaMultiplier = (float) Easings.BounceEaseInOut(progress);
                 else
                     alphaMultiplier = MathF.Round(progress, 0);
             }
