72d753dbfa5602f5eb9d4f487e35c6417425ca0a
Diogo Trindade <diogotr7@gmail.com>
2021-10-07 21:14:25 +0100

    Fixed Lgs prerequisite

--- src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/LgsOrGhubPrerequisite.cs
+++ src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/LgsOrGhubPrerequisite.cs
@@ -10,20 +10,24 @@ namespace Artemis.Plugins.Devices.Logitech.Prerequisites
         public LgsOrGhubPrerequisite(Plugin plugin)
         {
             _plugin = plugin;
+
+            InstallActions = new()
+            {
+                new DownloadFileAction("Download LGHUB installer", "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe", _plugin.ResolveRelativePath("lghub_installer.exe")),
+                new ExecuteFileAction("Install LGHUB", _plugin.ResolveRelativePath("lghub_installer.exe"), null, true, true),
+                new DeleteFileAction("Delete LGHUB installer", _plugin.ResolveRelativePath("lghub_installer.exe")),
+            };
+
+            UninstallActions = new();
         }
 
         public override string Name => "Required Logitech software";
 
         public override string Description => "Checks if the required Logitech software is installed on the system.";
 
-        public override List<PluginPrerequisiteAction> InstallActions => new()
-        {
-            new DownloadFileAction("Download LGHUB installer", "https://download01.logi.com/web/ftp/pub/techsupport/gaming/lghub_installer.exe", _plugin.ResolveRelativePath("lghub_installer.exe")),
-            new ExecuteFileAction("Install LGHUB", _plugin.ResolveRelativePath("lghub_installer.exe"), null, true, true),
-            new DeleteFileAction("Delete LGHUB installer", _plugin.ResolveRelativePath("lghub_installer.exe")),
-        };
+        public override List<PluginPrerequisiteAction> InstallActions { get; }
 
-        public override List<PluginPrerequisiteAction> UninstallActions => new();
+        public override List<PluginPrerequisiteAction> UninstallActions { get; }
 
         public override bool IsMet()
         {
