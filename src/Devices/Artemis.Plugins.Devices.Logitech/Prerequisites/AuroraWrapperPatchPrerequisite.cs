ed6d8fa34be688be5009277c2cba0da448a2c010
Diogo Trindade <diogotr7@gmail.com>
2021-10-19 14:48:35 +0100

    Fixed capitalisation, added missing dispose

--- src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/AuroraWrapperPatchPrerequisite.cs
+++ src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/AuroraWrapperPatchPrerequisite.cs
@@ -1,7 +1,5 @@
 ﻿using Artemis.Core;
-using Microsoft.Win32;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 
 namespace Artemis.Plugins.Devices.Logitech.Prerequisites
@@ -11,20 +9,20 @@ namespace Artemis.Plugins.Devices.Logitech.Prerequisites
     /// </summary>
     internal class AuroraWrapperPatchPrerequisite : PluginPrerequisite
     {
-        public override string Name => "Logitech Dll Check";
+        public override string Name => "Logitech DLL Check";
 
-        public override string Description => "Checks for presence of the backup logitech dll placed by Aurora";
+        public override string Description => "Checks for presence of the backup Logitech DLL placed by Aurora";
 
         public override List<PluginPrerequisiteAction> InstallActions { get; } = new()
         {
-            new RunInlinePowerShellAction("Copy backup dll over", $"Move-Item -Path \"{AURORA_BACKUP_PATH}\" -Destination \"{AURORA_WRAPPER_PATH}\"" , true)
+            new RunInlinePowerShellAction("Copy backup DLL over", $"Move-Item -Path \"{AURORA_BACKUP_PATH}\" -Destination \"{AURORA_WRAPPER_PATH}\"" , true)
         };
 
         public override List<PluginPrerequisiteAction> UninstallActions { get; } = new();
 
         public override bool IsMet() => !File.Exists(AURORA_BACKUP_PATH);
 
-        private const string AURORA_WRAPPER_PATH = "C:\\Program Files\\Logitech Gaming Software\\SDK\\LED\\x64\\LogitechLed.dll";
-        private const string AURORA_BACKUP_PATH = "C:\\Program Files\\Logitech Gaming Software\\SDK\\LED\\x64\\LogitechLed.dll.aurora_backup";
+        private const string AURORA_WRAPPER_PATH = @"C:\Program Files\Logitech Gaming Software\SDK\LED\x64\LogitechLed.dll";
+        private const string AURORA_BACKUP_PATH = @"C:\Program Files\Logitech Gaming Software\SDK\LED\x64\LogitechLed.dll.aurora_backup";
     }
 }
--- src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/LogitechSoftwareChecker.cs
+++ src/Devices/Artemis.Plugins.Devices.Logitech/Prerequisites/LogitechSoftwareChecker.cs
@@ -4,21 +4,20 @@ namespace Artemis.Plugins.Devices.Logitech.Prerequisites
 {
     internal static class LogitechSoftwareChecker
     {
-        internal static bool IsLgsInstalled()
+        internal static bool IsLgsInstalled() => DoesRegistrySubKeyExist(REGISTRY_LGS);
+
+        internal static bool IsLghubInstalled() => DoesRegistrySubKeyExist(REGISTRY_LGHUB);
+
+        internal static bool DoesRegistrySubKeyExist(string subkey)
         {
             using RegistryKey installedPrograms = Registry.LocalMachine.OpenSubKey(REGISTRY_INSTALLED_SOFTWARE);
 
-            return installedPrograms.OpenSubKey(REGISTRY_LGS) != null;
+            using RegistryKey subKey = installedPrograms.OpenSubKey(subkey);
+
+            return subkey != null;
         }
 
-        internal static bool IsLghubInstalled()
-        {
-            using RegistryKey installedPrograms = Registry.LocalMachine.OpenSubKey(REGISTRY_INSTALLED_SOFTWARE);
-
-            return installedPrograms.OpenSubKey(REGISTRY_LGHUB) != null;
-        }
-
-        private const string REGISTRY_INSTALLED_SOFTWARE = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall";
+        private const string REGISTRY_INSTALLED_SOFTWARE = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
         private const string REGISTRY_LGHUB = "{521c89be-637f-4274-a840-baaf7460c2b2}";
         private const string REGISTRY_LGS = "Logitech Gaming Software";
     }
