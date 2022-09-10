a629cfe50a8e9cd12061696658fadce9cb7bf60c
Diogo Trindade <diogotr7@gmail.com>
2022-09-10 18:41:34 +0100

    Processes - Fixed NRE when alt-tabbing quickly

--- src/Modules/Artemis.Plugins.Modules.Processes/DataModels/WindowDataModel.cs
+++ src/Modules/Artemis.Plugins.Modules.Processes/DataModels/WindowDataModel.cs
@@ -11,43 +11,8 @@ namespace Artemis.Plugins.Modules.Processes.DataModels;
 
 public class WindowDataModel
 {
-    private readonly IWindowService _windowService;
-
-    public WindowDataModel(Process process, IColorQuantizerService quantizerService, IWindowService windowService)
-    {
-        Process = process;
-        ProcessName = process.ProcessName;
-
-        // Accessing MainModule requires admin privileges, this way does not
-        ProgramLocation = process.GetProcessFilename();
-
-        // Get Icon colors
-        if (!File.Exists(ProgramLocation))
-            return;
-        using MemoryStream stream = new();
-        Icon.ExtractAssociatedIcon(ProgramLocation)?.Save(stream);
-        stream.Seek(0, SeekOrigin.Begin);
-        using SKBitmap bitmap = SKBitmap.FromImage(SKImage.FromEncodedData(stream));
-        stream.Close();
-        if (bitmap == null)
-            return;
-
-        SKColor[] colors = quantizerService.Quantize(bitmap.Pixels, 256);
-        Colors = quantizerService.FindAllColorVariations(colors, true);
-
-        _windowService = windowService;
-    }
-
-    [DataModelIgnore]
-    public Process Process { get; }
-
     public string WindowTitle { get; set; }
     public string ProcessName { get; set; }
     public string ProgramLocation { get; set; }
     public ColorSwatch Colors { get; set; }
-
-    public void UpdateWindowTitle()
-    {
-        WindowTitle = _windowService.GetActiveWindowTitle();
-    }
 }
