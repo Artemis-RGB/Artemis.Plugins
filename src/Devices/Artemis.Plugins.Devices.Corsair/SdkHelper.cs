a4f2fe9fbb727f89e40e5bb231b1c64fad68c7f3
Robert <mail@rbeekman.nl>
2024-06-19 20:45:45 +0200

    Corsair - Add extra registry key for iCUE 5 install location

--- src/Devices/Artemis.Plugins.Devices.Corsair/SdkHelper.cs
+++ src/Devices/Artemis.Plugins.Devices.Corsair/SdkHelper.cs
@@ -9,34 +9,41 @@ namespace Artemis.Plugins.Devices.Corsair;
 
 public static class SdkHelper
 {
-    private const string ICUE_REGISTRY_KEY = @"HKEY_LOCAL_MACHINE\SOFTWARE\Corsair\Corsair Utility Engine";
-    
+    private static string[] _registryKeys =
+    [
+        @"HKEY_LOCAL_MACHINE\SOFTWARE\Corsair\Corsair Utility Engine", // iCUE 4
+        @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\{A9B0B2D7-8C59-4413-A2FB-99EDBE65A608}" // iCUE 5
+    ];
+
     public static void EnsureSdkAvailable(ILogger logger)
     {
         // If iCUE is running, the SDK is available
         if (Process.GetProcessesByName("iCUE").Length > 0)
             return;
-        
+
         string? icuePath = GetICuePath();
         if (icuePath == null)
             throw new ArtemisPluginException("iCUE does not seem to be installed.");
 
         string path = Path.Combine(icuePath, "iCUE.exe");
         logger.Information("Starting iCUE at {Path}", path);
-        
+
         ProcessStartInfo startInfo = new() {FileName = Path.Combine(icuePath, "iCUE.exe"), Arguments = "--autorun", UseShellExecute = true};
         Process.Start(startInfo);
-        
-        logger.Information("Waiting 8 sec for the iCUE SDK to become available...");
-        Thread.Sleep(8000);
+
+        logger.Information("Waiting 10 sec for the iCUE SDK to become available...");
+        Thread.Sleep(10000);
     }
-    
+
     private static string? GetICuePath()
     {
-        // The UninstallString is the path to the uninstaller, we need to extract the path to the executable
-        if (Registry.GetValue(ICUE_REGISTRY_KEY, "InstallLocation", null) is not string installLocation)
-            return null;
+        foreach (string registryKey in _registryKeys)
+        {
+            // The UninstallString is the path to the uninstaller, we need to extract the path to the executable
+            if (Registry.GetValue(registryKey, "InstallLocation", null) is string installLocation)
+                return installLocation;
+        }
 
-        return installLocation;
+        return null;
     }
 }
