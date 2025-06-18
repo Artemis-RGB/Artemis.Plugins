dcc5e6c60e15a65b659b6d6a157996af8bbbf72d
Diogo Trindade <diogotr7@gmail.com>
2025-06-18 19:09:58 +0100

    grpc client

--- src/Devices/Artemis.Plugins.Devices.Wooting/WootingDeviceProvider.cs
+++ src/Devices/Artemis.Plugins.Devices.Wooting/WootingDeviceProvider.cs
@@ -3,7 +3,7 @@ using Artemis.Core.DeviceProviders;
 using Artemis.Core.Services;
 using RGB.NET.Core;
 using Serilog;
-using RGBDeviceProvider = RGB.NET.Devices.Wooting.WootingDeviceProvider;
+using RGBDeviceProvider = RGB.NET.Devices.Wooting.Grpc.WootingGrpcDeviceProvider;
 
 namespace Artemis.Plugins.Devices.Wooting
 {
@@ -24,9 +24,9 @@ namespace Artemis.Plugins.Devices.Wooting
 
         public override void Enable()
         {
-            RGBDeviceProvider.PossibleX64NativePathsWindows.Add(Path.Combine(Plugin.Directory.FullName, "x64", "wooting-rgb-sdk64.dll"));
-            RGBDeviceProvider.PossibleX86NativePathsWindows.Add(Path.Combine(Plugin.Directory.FullName, "x86", "wooting-rgb-sdk.dll"));
-            RGBDeviceProvider.PossibleNativePathsLinux.Add(Path.Combine(Plugin.Directory.FullName, "x64", "libwooting-rgb-sdk.so"));
+            // RGBDeviceProvider.PossibleX64NativePathsWindows.Add(Path.Combine(Plugin.Directory.FullName, "x64", "wooting-rgb-sdk64.dll"));
+            // RGBDeviceProvider.PossibleX86NativePathsWindows.Add(Path.Combine(Plugin.Directory.FullName, "x86", "wooting-rgb-sdk.dll"));
+            // RGBDeviceProvider.PossibleNativePathsLinux.Add(Path.Combine(Plugin.Directory.FullName, "x64", "libwooting-rgb-sdk.so"));
 
             RgbDeviceProvider.Exception += Provider_OnException;
             _deviceService.AddDeviceProvider(this);
