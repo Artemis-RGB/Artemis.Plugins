0cfbaff2789e7de3eda5b079ce6dd3258a650b80
Fiveol <andyinventor@outlook.com>
2025-12-18 18:51:32 -0800

    Update PerformanceModule.cs

--- src/Modules/Artemis.Plugins.Modules.Performance/PerformanceModule.cs
+++ src/Modules/Artemis.Plugins.Modules.Performance/PerformanceModule.cs
@@ -22,7 +22,7 @@ public class PerformanceModule : Module<PerformanceDataModel>
     public override void Enable()
     {
         //TODO: Make update frequency configurable
-        AddTimedUpdate(TimeSpan.FromMilliseconds(500), _ => UpdatePerformance(), "UpdatePerformance");
+        AddTimedUpdate(TimeSpan.FromMilliseconds(1000), _ => UpdatePerformance(), "UpdatePerformance");
     }
 
     public override void Disable()
